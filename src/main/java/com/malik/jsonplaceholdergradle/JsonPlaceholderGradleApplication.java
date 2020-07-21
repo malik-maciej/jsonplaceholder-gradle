1e82e2e2f156846e735c8a5d8f7f547647ef2d11
Maciej Malik <malik-maciej@o2.pl>
2020-07-22 00:08:51 +0200

    fix name

---
+++ src/main/java/com/malik/jsonplaceholdergradle/JsonPlaceholderGradleApplication.java
@@ -0,0 +1,12 @@
+package com.malik.jsonplaceholdergradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonPlaceholderGradleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonPlaceholderGradleApplication.class, args);
+    }
+}
